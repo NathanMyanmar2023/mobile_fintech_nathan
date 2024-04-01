50653e2b68e098b46248c2c57309509adc3787ee
Thae00 <mathaeoo97@gmail.com>
2024-04-01 15:17:23 +0630

    release of version 1.0.2

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,10 +4,10 @@ class Config {
   static const httpRequestTimeout = 15;
 
   /// Production
-  // static const String productionUrl = "https://nathanfintech.com/";
+   static const String productionUrl = "https://nathanfintech.com/";
 
   /// uat testing
-  static const String productionUrl = "http://thecreatorcreates.com/";
+  //static const String productionUrl = "http://thecreatorcreates.com/";
 
   static const bool isLiveVoucher = false;
 }
